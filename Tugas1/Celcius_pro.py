a5cf292d5b612a7c4ce36a64dcfc0d0a7a09b6a0
IrvanAfriandi <127779357+IrvanAfriandi@users.noreply.github.com>
2023-03-14 13:00:30 +0700

    Add files via upload

---
+++ Tugas1/Celcius_pro.py
@@ -0,0 +1,27 @@
+class Celcius:
+    @staticmethod
+    def Fahrenheit(Cel):
+        C = Cel
+        return (9/5) * C + 32
+    @staticmethod
+    def Kelvin(Cel):
+        C = Cel
+        return C + 273
+    @staticmethod
+    def Reamur(Cel):
+        C = Cel
+        return 4/5 * C
+    
+CF = 75
+CR = 60
+CK = 90
+
+
+fahrenheit = Celcius.Fahrenheit(CF)
+reamur = Celcius.Reamur(CR) 
+kelvin = Celcius.Kelvin(CK)
+
+print("Konversi ",CF, "derajat Celcius adalah ",fahrenheit, "derajat Fahrenheit")
+print("Konversi ",CR, "derajat Celcius adalah ",reamur, "derajat Reamur")
+print("Konversi ",CK, "derajat Celcius adalah ",kelvin, "derajat Kelvin")
+
