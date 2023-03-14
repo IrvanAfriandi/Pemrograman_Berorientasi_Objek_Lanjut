b9d7e141dbde376f2b30a3336236467fde7dae40
IrvanAfriandi <127779357+IrvanAfriandi@users.noreply.github.com>
2023-03-14 11:46:05 +0700

    Delete Latihan_1 directory

--- Latihan_1/Contoh 4.py
+++
@@ -1,9 +0,0 @@
-class Buku:
-    def __init__(self, judul, penulis):
-        self.judul = judul
-        self.penulis = penulis
-    def info(self):
-        print(f"Judul: {self.judul}\nPenulis: {self.penulis}")
-
-bukuA = Buku("Tenggelamnya Kapal Van Der Wijck", "Haji Abdul Malik")
-bukuA.info()
