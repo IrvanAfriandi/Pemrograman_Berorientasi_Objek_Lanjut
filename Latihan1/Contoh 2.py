064030c2e0e536ed53d1058a0afca21ecc011410
IrvanAfriandi <127779357+IrvanAfriandi@users.noreply.github.com>
2023-03-17 00:42:01 +0700

    Delete Latihan1 directory

--- Latihan1/Contoh 2.py
+++
@@ -1,9 +0,0 @@
-class Mahasiswa:
-    def __init__(self, nama, npm):
-        self.nama = nama
-        self.npm = npm
-    def info(self):
-        print(f"Nama: {self.nama}\nNPM: {self.npm}")
-
-mahasiswaB = Mahasiswa("Joko Wijayanto", "31003313")
-mahasiswaB.info()
